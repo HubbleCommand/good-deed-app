aa8983c8d9999573ea87b3bcf86f43a3380ce258
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-01-24 21:53:54 +0100

    Null safety + Update Post Data Models

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:good_deed/models/user.dart';
 import 'package:geotools/geotools.dart';
 
@@ -6,15 +8,21 @@ class Post {
   final User user;
   final LatLong location;
   final int time;
+  final String title;
+  final String content;
+  final List<String> pictures;
 
-  Post({ this.postId, this.user, this.location, this.time });
+  Post({ this.postId, this.user, this.location, this.time, this.title, this.content, this.pictures });
 
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
-      postId    : json['postId'],
-      user    : json['user'],
-      location  : json['location'],
-      time      : json['time']
+      postId    : json['postId'] as int,
+      time      : json['time'] as int,
+      user      : json['deeded'] ? User.fromJson((json['deeded'])) : null,
+      location  : json['location'] ? LatLong.fromJson((json['location'])) : null,
+      title     : json['title'] as String,
+      content   : json['content'] as String,
+      pictures  : jsonDecode(json['time']) as List<String>,
     );
   }
 }
