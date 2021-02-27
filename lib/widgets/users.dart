24a986c8eea05d358a563657d39fa002bff15efc
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-02-27 22:29:25 +0100

    Users page list sample...

--- lib/widgets/users.dart
+++ lib/widgets/users.dart
@@ -10,24 +10,30 @@ class UsersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: AppBar(
-          title: Text("Users"),
-        ),
-        drawer: GDDrawer(),
-        body: Center(child: Text("Users")));
-  }
-}
-
-class AccountPage extends StatelessWidget {
-  static const String routeName = '/account';
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text("Account"),
-        ),
-        drawer: GDDrawer(),
-        body: Center(child: Text("Account")));
+      appBar: AppBar(
+        title: Text("Users"),
+      ),
+      drawer: GDDrawer(),
+      //body: Center(child: Text("Users")));
+      body: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Text('Users...'),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text('Filter'),
+                ),
+              ),
+            ],
+          )
+      ),
+    );
   }
 }
