a105d309109e42da9eda950cec71501d2ff37881
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-02-28 11:56:25 +0100

    Deeds List code clean

--- lib/widgets/deeds.dart
+++ lib/widgets/deeds.dart
@@ -28,7 +28,7 @@ class DeedsPage extends StatelessWidget {
             children: <Widget>[
               Expanded(
                 //child: Deeds(),
-                child: DeedsList2(),
+                child: DeedsList(),
               ),
             ]
         ),
@@ -48,54 +48,14 @@ class DeedsPage extends StatelessWidget {
 }
 
 //TODO look at this: https://medium.com/@sharmadhiraj.np/infinite-scrolling-listview-on-flutter-88d7a5e2bb4
-List<Deed> parseDeeds(String responseBody) {
-  print('PARSING...');
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  print('DECODED DEEDS');
-
-  var calced = parsed.map<Deed>((json) => Deed.fromJson(json)).toList();
-  print('DID LAST THING');
-  return calced;
-}
-
-Future<List<Deed>> fetchDeeds() async {
-  final response = await http.Client().get('http://192.168.1.33:3000/deeds');
-  print('GOT DEEDS');
-  return compute(parseDeeds, response.body);
-}
-
-//TODO make stateful!
-class Deeds extends StatelessWidget{
-  Deeds({Key key}) : super(key: key);
+class DeedsList extends StatefulWidget {
+  DeedsList({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    /*return Scaffold(
-      body: FutureBuilder<List<Deed>>(
-        future: fetchDeeds(http.Client()),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? DeedsList2(deeds: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
-      ),
-    );*/
-    return Scaffold(
-      body: DeedsList2()
-    );
-  }
+  DeedsListState createState() => DeedsListState();
 }
 
-class DeedsList2 extends StatefulWidget {
-  DeedsList2({Key key}) : super(key: key);
-
-  @override
-  DeedsList2State createState() => DeedsList2State();
-}
-
-class DeedsList2State extends State<DeedsList2> {
+class DeedsListState extends State<DeedsList> {
   bool _hasMore;
   int _pageNumber;
   bool _error;
@@ -108,6 +68,8 @@ class DeedsList2State extends State<DeedsList2> {
 
   int _timeRequest;
 
+  final _filterFormKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -119,12 +81,78 @@ class DeedsList2State extends State<DeedsList2> {
 
     _timeRequest = DateTime.now().millisecondsSinceEpoch;
 
-    fetchDeeds(10, _timeRequest);
+    _fetchDeeds(10, _timeRequest);
   }
 
   @override
   Widget build(BuildContext context) {
-    return getBody();
+    //return getBody();
+    return Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: getBody(),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return Dialog(
+                          child: Stack(
+                            children: [
+                              Text(
+                                'Filter Deeds',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                ),
+                              ),
+                              _buildFilterForm(_filterFormKey)
+                            ],
+                          ),
+                        );
+                      }
+                  );
+                  // Respond to button press
+                },
+                child: Text('Filter'),
+              ),
+            ),
+          ]
+      ),
+    );
+  }
+
+  _buildFilterForm(Key key){
+    return Form(
+        key: key,
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.title),
+                      labelText: 'Title'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (String value){/*title=value;*/},
+                ),
+              ),
+            ]
+        )
+    );
   }
 
   Widget getBody() {
@@ -142,7 +170,7 @@ class DeedsList2State extends State<DeedsList2> {
                 setState(() {
                   _loading = true;
                   _error = false;
-                  fetchDeeds(10, _timeRequest);
+                  _fetchDeeds(10, _timeRequest);
                 });
               },
               child: Padding(
@@ -157,7 +185,7 @@ class DeedsList2State extends State<DeedsList2> {
           itemBuilder: (context, index) {
             //if (index == futureDeeds2.length - _nextPageThreshold) {
             if (index == futureDeeds2.length - _nextPageThreshold && timesFoundZeroDeeds < timesFoundZeroDeedsThreshold) {
-              fetchDeeds(10, _timeRequest);
+              _fetchDeeds(10, _timeRequest);
             }
             if(timesFoundZeroDeeds >= timesFoundZeroDeedsThreshold){
               //TODO make an end item to show that at end of list : https://flutter.dev/docs/cookbook/lists/mixed-list
@@ -172,7 +200,7 @@ class DeedsList2State extends State<DeedsList2> {
                         setState(() {
                           _loading = true;
                           _error = false;
-                          fetchDeeds(10, _timeRequest);
+                          _fetchDeeds(10, _timeRequest);
                         });
                       },
                       child: Padding(
@@ -195,7 +223,17 @@ class DeedsList2State extends State<DeedsList2> {
     return Container();
   }
 
-  Future<void> fetchDeeds(int limit, int before) async {
+  List<Deed> _parseDeeds(String responseBody) {
+    print('PARSING...');
+    final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+    print('DECODED DEEDS');
+
+    var calced = parsed.map<Deed>((json) => Deed.fromJson(json)).toList();
+    print('DID LAST THING');
+    return calced;
+  }
+
+  Future<void> _fetchDeeds(int limit, int before) async {
     try {
       int skip = (_pageNumber) * 10; //TODO needs to use the actual number of deeds!
       skip = futureDeeds2.length;
@@ -212,7 +250,7 @@ class DeedsList2State extends State<DeedsList2> {
       final response = await http.Client().get(url);
       print('GOT DEEDS');
       //return compute(parseDeeds, response.body);
-      List<Deed> parsedDeeds = parseDeeds(response.body);
+      List<Deed> parsedDeeds = _parseDeeds(response.body);
       print('Number of deeds found: ' + parsedDeeds.length.toString());
 
       if(parsedDeeds.length == 0){
@@ -234,22 +272,6 @@ class DeedsList2State extends State<DeedsList2> {
         _error = true;
       });
     }
-  }
-}
-
-class DeedsList extends StatelessWidget {
-  final List<Deed> deeds;
-
-  DeedsList({Key key, this.deeds}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: deeds.length,
-      itemBuilder: (context, index) {
-        return DeedItem(deeds[index]);
-      },
-    );
   }
 }
 
@@ -317,194 +339,6 @@ class DeedItem extends StatelessWidget {
       onTap: () {
         Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new DeedPage(deed: _deed)));
       },
-    );
-  }
-
-}
-
-//TODO turn to stateless? Maybe not, as setState() is easy way to redraw / add elements to list without rebuilding / re-rendering everything / whole list!
-class DeedsOld extends StatefulWidget {
-  @override
-  _DeedsState createState() => _DeedsState();
-}
-
-class _DeedsState extends State<DeedsOld> {
-  final _deeds = <Deed>[];
-  //final _saved = Set<WordPair>();
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  final LatLong statueOfLiberty = LatLong.fromDecimal(40.68972222, 72.04444444);  //Mocked User Location
-  //String title;
-  //String description;
-
-  Widget _buildRow(Deed deed) {
-    //final alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      //dense:true, //Makes stuff closer together & text smaller
-      //isThreeLine: true, //Gives more space for subtitle (here, description), however can fuck / not fuck with other formatting (i.e. trailing size)
-      //Distance from user
-      leading: Container(
-        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              (GeoUtils.distanceInMeters(statueOfLiberty, deed.location) / 1000).roundToDouble().toString() + ' km',
-              textAlign: TextAlign.center,
-            ),
-          ],
-        )
-      ),
-      //Title
-      title: Text(
-        deed.title.length > 10 ? deed.title.substring(0, 9) + '...' : deed.title,
-        style: _biggerFont,
-      ),
-      //Description
-      subtitle: Text(
-        deed.description.length > 15 ? deed.description.substring(0, 14) + '...' : deed.description,
-      ),
-      //Trailing User Avatar images
-      trailing: Container(
-        child: new Stack(
-          clipBehavior:Clip.none,
-          children: <Widget>[
-            ImageUtils.Image.buildIcon(deed.deeder.avatar, 36.0, 36.0),
-            new Positioned(
-              left: 20.0,
-              child:ImageUtils.Image.buildIcon(deed.deeded.avatar, 36.0, 36.0),
-            ),
-          ],
-        ),
-      ),
-      onTap: () {
-        Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new DeedPage(deed: deed)));
-      },
-    );
-  }
-
-  Widget _buildDeeds() {
-    //https://flutter.dev/docs/cookbook/lists/long-lists
-    //So, based on these:
-    //  -https://googleflutter.com/flutter-add-item-to-listview-dynamically/
-    //  -https://stackoverflow.com/questions/51343567/append-items-dynamically-to-listview
-    //Setting state to update _deeds will add items, so pretty easy?
-    //Just need to add controller, so that can see when close to bottom to retrieve like 10 more deeds
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemCount: _deeds.length,
-        itemBuilder: (context, i) {
-          return _buildRow(_deeds[i]);
-        }
-      );
-  }
-
-  _buildFilterForm(Key key){
-    return Form(
-      key: key,
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.title),
-                      labelText: 'Title'
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                  onFieldSubmitted: (String value){/*title=value;*/},
-                ),
-              ),
-            ]
-        )
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //Build sample deeds
-    //TODO use http or dio to get deeds, with filters
-    _deeds.clear(); //Need to clear the current deeds? TODO make sure this works well with setState(), or just call it first in setState()!
-    for (var i = 0; i < 10; i++) {
-      _deeds.add(
-          new Deed(
-              deedId          : i,
-              deeder          : new User(
-                userId  : 1,
-                name    : 'Bob',
-                contact : 'bob@gmail.com',
-                home    : LatLong.fromDecimal(48.85805556, 2.29416667),
-                avatar: 'https://image.shutterstock.com/image-vector/user-icon-man-business-suit-600w-1700749465.jpg',
-              ),
-              deeded          : new User(
-                userId  : 2,
-                name    : 'Alice',
-                contact : 'alice@gmail.com',
-                home    : LatLong.fromDecimal(48.85805556, 2.29416667),
-                avatar: 'https://image.shutterstock.com/image-vector/business-woman-icon-avatar-symbol-600w-790518412.jpg',
-              ),
-              location        : new LatLong.fromDecimal(48.85805556, 2.29416667),
-              time            : 1,
-              title           : "Deed " + i.toString(),
-              description     : "Deed description " + i.toString(),
-              //picture         : 'https://i.imgur.com/BoN9kdC.png'
-              pictures: ['https://picsum.photos/200']
-          )
-      );
-    }
-
-    final _formKey = GlobalKey<FormState>();
-    final _filterFormKey = GlobalKey<FormState>();
-
-    //TODO Most of this needs to be moved! Only keep the Expanded with _buildDeeds() as child! -> Everything else is NOT related to displaying deeds, which is the purpose of this stateful widget!
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            child: _buildDeeds(),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context){
-                    return Dialog(
-                      child: Stack(
-                        children: [
-                          Text(
-                            'Filter Deeds',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                            ),
-                          ),
-                          _buildFilterForm(_filterFormKey)
-                        ],
-                      ),
-                    );
-                  }
-                );
-                // Respond to button press
-              },
-              child: Text('Filter'),
-            ),
-          ),
-        ]
-      ),
     );
   }
 }
