ce310c74a33a70ab391d602a43438626f2116b7e
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-04-21 23:24:19 +0200

    User model update w userID as String

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,20 +1,26 @@
-import 'package:geotools/geotools.dart';
+import 'package:latlong/latlong.dart';
 
 class User { // / or Deedee
-  final int userId;
+  final String userId;  //ID MUST be a string, as Firebase user ID is a string https://firebase.google.com/docs/reference/unity/class/firebase/auth/user-info-interface
   final String name;
   final String contact;
-  final LatLong home;
+  final LatLng home;
   final String avatar;
 
   User({this.userId, this.name, this.contact, this.home, this.avatar});
 
+  @override
+  String toString() {
+    return 'User: {name: $name, id: $userId}';
+  }
+
   factory User.fromJson(Map<String, dynamic> json) {
     return new  User(
-      userId  : json['userId'] as int,
+      userId  : json['userId'] as String,
       name    : json['name'] as String,
       contact : json['contact'] as String,
-      home    : json.containsKey('home') ? LatLong.fromJson(json['home']) : null,
+      //home    : json.containsKey('home') ? LatLong.fromJson(json['home']) : null,
+      home    : json.containsKey('home') ? LatLng(json['home']['lat'], json['home']['long']) : null,
       avatar  : json['avatar'] as String,
     );
   }
