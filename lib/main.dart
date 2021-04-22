9830783a1680109400533a92b6bb7799518c66ea
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-04-22 23:23:46 +0200

    Main uses global theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:good_deed/globals.dart';
 import 'package:good_deed/routes/Routes.dart';
 
 import 'package:good_deed/widgets/home.dart';
@@ -39,7 +40,7 @@ class GoodDeed extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.yellow,
+        primarySwatch: Globals.styles.mainThemeColor,
       ),
       home: MyHomePage(title: 'Good Deed Home Page'),
       /*routes:  {
