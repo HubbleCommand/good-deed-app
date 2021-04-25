4349a59ee266f41a65994644879735ee2daf3847
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-04-25 12:15:31 +0200

    Post view

---
+++ lib/widgets/views/post.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:good_deed/models/deed.dart';
+import 'package:good_deed/models/filters/deed.dart';
+import 'package:good_deed/models/filters/post.dart';
+import 'package:good_deed/models/filters/user.dart';
+import 'package:good_deed/models/post.dart';
+import 'package:good_deed/models/user.dart';
+import 'package:good_deed/utils/image.dart' as ImageUtils;
+import 'package:good_deed/widgets/deeds.dart';
+import 'package:good_deed/widgets/posts.dart';
+import 'package:good_deed/widgets/users.dart';
+import 'package:good_deed/utils/layout.dart';
+import 'package:good_deed/widgets/views/user.dart';
+
+class PostPage extends StatelessWidget {
+  final _titleStyle = TextStyle(fontSize: 35.0);
+  final _descriptionStyle = TextStyle(fontSize: 15.0);
+  static const String routeName = '/post';
+  final Post post;
+
+  PostPage({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text(post.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16.0),
+          ),
+          ImageUtils.Image.buildIcon(post.pictures.first, 190.0, 190.0),
+          LayoutUtils.splitter(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //Buttons for Deeds where is Deeder, Deeded, and Posts by user
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new UserPage(user: post.user)));
+                },
+                child: Text('Poster'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
