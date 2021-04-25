dbef6e00fa55a78737841e80a6eab2b6d736c486
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-04-25 12:19:56 +0200

    Post model location fixes + pictures list fixes

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 
 import 'package:good_deed/models/user.dart';
-import 'package:geotools/geotools.dart';
+import 'package:latlong/latlong.dart';
 
 class Post {
   final int postId;
   final User user;
-  final LatLong location;
+  final LatLng location;
   final int time;
   final String title;
   final String content;
@@ -19,10 +19,15 @@ class Post {
       postId    : json['postId'] as int,
       time      : json['time'] as int,
       user      : json.containsKey('user') ?  User.fromJson(json['user']) : null,
-      location  : json.containsKey('location') ? LatLong.fromJson(json['location']) : null,
+      //location  : json.containsKey('location') ? LatLng.fromJson(json['location']) : null,
+      location  : json.containsKey('location') ? LatLng(json['location']['lat'], json['location']['long']) : null,
       title     : json['title'] as String,
       content   : json['content'] as String,
-      pictures  : List.from(json['pictures']) as List<String>,
+      //pictures  : List.from(json['pictures']) as List<String>,
+      /*pictures: List.fromMap(Map<String, dynamic> data) {
+        likes = data['likes'].cast<String>();
+      }*/
+      pictures: json['pictures'].cast<String>()
     );
   }
 }
