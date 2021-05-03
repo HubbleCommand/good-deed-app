c4d64fc64f4658b3c6d96b2e926f08afc943f337
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-03 14:57:10 +0200

    Globals mocked user & home fix

--- lib/globals.dart
+++ lib/globals.dart
@@ -15,10 +15,13 @@ class Globals{
   //Mocked user's password: f@Xp4X=h5vB!4nN-
   static final mockedUser = new User(
       name: 'Mocked',
-      userId: 'NwXtTpAd5VvWP8eo',
-      avatar: 'https://www.clickz.com/wp-content/uploads/2016/03/anontumblr-300x271.png',
-      home: new LatLng(40.68972222, 72.04444444), //Mocked User Location, statueOfLiberty
+      uuid: 'NwXtTpAd5VvWP8eo',
+      avatarURL: 'https://www.clickz.com/wp-content/uploads/2016/03/anontumblr-300x271.png',
+      story: "I am a mocked user. Don't mind me!"
+      //home: new LatLng(40.68972222, 72.04444444), //Mocked User Location, statueOfLiberty
   );
+
+  static final mockedHome = new LatLng(40.68972222, 72.04444444); //Mocked User Location, statueOfLiberty
 
   static _Styles styles = _Styles();
 }
