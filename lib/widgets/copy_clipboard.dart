c1b73916a097ab403ef98e72d3c3235aca91f31e
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-11 22:02:17 +0200

    Account view update

---
+++ lib/widgets/copy_clipboard.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CopyToClipboardWidget extends StatelessWidget{
+  CopyToClipboardWidget({@required this.onTapCopy, @required this.message});
+  final Function onTapCopy;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        var text = onTapCopy();
+        // Respond to button press
+        print('Received text $text');
+        ClipboardData data = ClipboardData(text: '$text');
+        await Clipboard.setData(data);
+
+        ScaffoldMessenger
+            .of(context)
+            .showSnackBar(SnackBar(content: Text(message)));
+      },
+      icon: Icon(Icons.sticky_note_2, size: 18),
+    );
+  }
+}
--- lib/widgets/views/account.dart
+++ lib/widgets/views/account.dart
@@ -1,22 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:good_deed/widgets/buttons/button_logout.dart';
-import 'package:good_deed/widgets/forms/login/email_login.dart';
-import 'package:good_deed/widgets/forms/signup/email_signup.dart';
-import 'package:good_deed/widgets/drawer.dart';
+import 'package:good_deed/widgets/copy_clipboard.dart';
 import 'package:good_deed/widgets/views/user.dart';
 import 'package:good_deed/models/user.dart' as GDUser;
 
 //TODO put this all in settings?
 class AccountView extends StatelessWidget {
-  //static const String routeName = '/account';
-
   @override
   Widget build(BuildContext context) {
     User fbUser = FirebaseAuth.instance.currentUser;
     return Column(
       children: [
-        Text(FirebaseAuth.instance.currentUser == null ? 'NOT LOGGED IN' : 'YOU ARE LOGGED IN'),
+        Text("Your ID is : "),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(FirebaseAuth.instance.currentUser.uid),
+            CopyToClipboardWidget(onTapCopy: (){return FirebaseAuth.instance.currentUser.uid;}, message: "Copied your User ID to your clipboard"),
+          ],
+        ),
+        Text("Share this with your friends so that they can easily find you, or keep it secret!", textAlign: TextAlign.center,),
+        Divider(),
         LogoutButton(),
         UserView(user: new GDUser.User(uuid: fbUser.uid, name: fbUser.displayName, avatarURL: fbUser.photoURL),)
       ],
