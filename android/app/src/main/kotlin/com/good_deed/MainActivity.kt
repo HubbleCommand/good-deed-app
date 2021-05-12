5df6f087bc8bffe788c0cd72f391f775bc5c4383
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-12 19:44:58 +0200

    Stuff fore Facebook login

--- android/app/src/main/kotlin/com/good_deed/MainActivity.kt
+++ android/app/src/main/kotlin/com/good_deed/MainActivity.kt
@@ -2,5 +2,8 @@ package com.good_deed
 
 import io.flutter.embedding.android.FlutterActivity
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 class MainActivity: FlutterActivity() {
 }
