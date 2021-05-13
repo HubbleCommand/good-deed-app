886822184db8f31583f97634aa505615972ce714
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-14 00:34:08 +0200

    User view posted by button & code refactor

--- lib/widgets/views/user.dart
+++ lib/widgets/views/user.dart
@@ -10,6 +10,15 @@ class UserView extends StatelessWidget{
 
   UserView({Key key, this.user}) : super(key: key);
 
+  Widget _buildDeedFilterButton({@required BuildContext context, String title, Function onPressed}){
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(title),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(  //Using ListView can utterly fuck up with sizing when this widget is used as a child! WTF! Why can't ListView take the parent sizing?!
@@ -23,18 +32,28 @@ class UserView extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             //Buttons for Deeds where is Deeder, Deeded, and Posts by user
-            ElevatedButton(
+            _buildDeedFilterButton(
+              context: context,
+              title: "Done Deeds",
               onPressed: () {
                 Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new DeedsPage(filterDeed: new FilterDeed(didders: [this.user]),)));
-              },
-              child: Text('Done Deeds'),
+              }
             ),
-            ElevatedButton(
+            _buildDeedFilterButton(
+              context: context,
+              title: "Recieved Deeds",
               onPressed: () {
                 Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new DeedsPage(filterDeed: new FilterDeed(gotters: [this.user]),)));
-              },
-              child: Text('Recieved Deeds'),
+              }
             ),
+            _buildDeedFilterButton(
+              context: context,
+              title: "Posted Deeds",
+              onPressed: () {
+                Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new DeedsPage(filterDeed: new FilterDeed(posters: [this.user]),)));
+              }
+            ),
+            this.user.story == null ? Container() : Text(this.user.story)
           ],
         ),
       ],
