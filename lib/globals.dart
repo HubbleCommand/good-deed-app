39609321ba5937811593d9a37a02181922fbec25
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-15 01:33:18 +0200

    Updated globals

--- lib/globals.dart
+++ lib/globals.dart
@@ -7,15 +7,18 @@ import 'package:good_deed/models/user.dart';
 
 //https://stackoverflow.com/questions/55243106/dart-nested-classes-how-to-access-child-class-variables
 class Globals{
-  static const String backendURL = 'http://192.168.1.33:3000';
+  static const String backendURL = 'http://192.168.1.29:3000';
+  static const String beDeedURI = '/deeds';
+  static const String beUserURI = '/users';
 
   //Styles
   //static const Color mainThemeColor = Colors.yellow;
 
   //Mocked user's password: f@Xp4X=h5vB!4nN-
   static final mockedUser = new User(
-      name: 'Mocked',
       uuid: 'i2gu41TWaJXpVYtsGxbnsvLw45J3',
+      name: 'Mocked',
+      contact: 'gd.mocked.user@gmail.com',
       avatarURL: 'https://www.clickz.com/wp-content/uploads/2016/03/anontumblr-300x271.png',
       story: "I am a mocked user. Don't mind me!"
       //home: new LatLng(40.68972222, 72.04444444), //Mocked User Location, statueOfLiberty
