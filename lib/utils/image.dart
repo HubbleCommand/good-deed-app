d990de21f8ee528b20043ec1ab8e3f674bf72b46
HubbleCommand <sasha.poirier@outlook.com>
2021-06-14 22:29:26 +0200

    Image util fixed if null source -> default

--- lib/utils/image.dart
+++ lib/utils/image.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 //TODO add to widgets? Not really a util...
 class ImageUtil {
   static Widget buildIcon(String source, num height, num width){
-    return Container(
-        width: width,
-        height: height,
-        decoration: new BoxDecoration(
-            shape: BoxShape.circle,
-            image: new DecorationImage(
-                fit: BoxFit.fill,
-                image: new NetworkImage(source)
-            )
-        )
-    );
+    if(source == null) {
+      return Icon(Icons.report, size: min(height, width));
+    } else {
+      return Container(
+          width: width,
+          height: height,
+          decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image: new NetworkImage(source)
+              )
+          )
+      );
+    }
   }
 }
