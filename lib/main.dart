a2a172d95d4ed2ad4a0054605efe2e7a34491926
HubbleCommand <sasha.poirier@outlook.com>
2022-02-27 22:36:21 +0100

    main Navigator 2.0 onGenerateRoute for web compatibility

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:good_deed/globals.dart';
 import 'package:good_deed/routes/Routes.dart';
+import 'package:good_deed/routes/deed.dart';
+import 'package:good_deed/routes/deeds.dart';
 import 'package:good_deed/routes/home.dart';
+import 'package:good_deed/routes/unknown.dart';
+import 'package:good_deed/routes/user.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 //import 'package:flutter_localizations/flutter_localizations.dart';
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
+//import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+//import '' if (kIsWeb) 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async{
+  if(kIsWeb){
+    //setUrlStrategy(PathUrlStrategy());
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
   //Initialise ad stuff, but ONLY if on web, and ONLY if on mobile
@@ -65,7 +75,8 @@ class GoodDeed extends StatelessWidget {
            */
         )
       ),
-      home: MyHomePage(title: 'Good Deed Home Page'),
+      //home: MyHomePage(title: 'Good Deed Home Page'),
+      initialRoute: MyHomePage.routeName, //replaces home
       /*routes:  {
         Routes.deeds: (context) => DeedsPage(),
         Routes.home: (context) => MyHomePage(title: 'Good Deed Home Page'),
@@ -74,6 +85,31 @@ class GoodDeed extends StatelessWidget {
         Routes.account: (context) => AccountPage(),
       },*/
       routes: Routes.routes,
+
+      //Navigator 2.0
+      //Flutter Router checks the home / initial route, then the routes table, then tries onGenerateRoute
+      //We ignore no return : if onGenerateRoute
+      // ignore: missing_return
+      onGenerateRoute: (settings) {
+        print('Looking for route');
+        print(settings);
+        var uri = Uri.parse(settings.name);
+        if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'deed') {
+          print('Deed URL id passed : ' + uri.pathSegments[1]);
+          var uuid = uri.pathSegments[1];
+          //OH MY GOD settings WAS MISSING, that's all you need to pass for it to update the URL
+          return MaterialPageRoute(settings: settings, builder: (context) => DeedPage(deedUUID: uuid));
+        }
+
+        /*if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'user') {
+          print('User URL id passed : ' + uri.pathSegments[1]);
+          return MaterialPageRoute(settings: settings, builder: (context) => UserPage(userUUID: uri.pathSegments[1]));
+        }*/
+
+        if(!Routes.routes.containsKey(settings.name)){
+          return MaterialPageRoute(settings: settings, builder: (context) => UnknownPage());
+        }
+      },
     );
   }
 }
