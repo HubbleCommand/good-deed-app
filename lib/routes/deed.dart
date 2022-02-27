f10fdaef7c2517d6d85931a649c3af1a371d03b8
HubbleCommand <sasha.poirier@outlook.com>
2022-02-27 10:54:34 +0100

    DeedPage now stateful, fetches deed if only id is passed

--- lib/routes/deed.dart
+++ lib/routes/deed.dart
@@ -1,23 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:good_deed/models/deed.dart';
 import 'package:good_deed/widgets/views/deed.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../globals.dart';
 
-class DeedPage extends StatelessWidget {
+//TODO convert DeedPage and UserPage to a single DetailPage
+//OR just remove these two
+//We may want to pass a whole deed, OR just the ID. If just the ID is passed, then we need to go fetch the deed
+
+class DeedPage extends StatefulWidget {
   static const String routeName = '/deed';
-  final Deed deed;
 
   final double userProfileIconDimensions = 25.0;
 
-  DeedPage({Key key, this.deed}) : super(key: key);
+  final Deed deed;
+  final String deedUUID;
+
+  DeedPage({Key key, this.deed, this.deedUUID}) : super(key: key);
+
+  @override
+  DeedPageState createState() => DeedPageState(deed: this.deed, deedUUID: this.deedUUID);
+}
+
+class DeedPageState extends State<DeedPage> {
+  Deed deed;
+  String deedUUID;
+
+  bool _error;
+  bool _loading = true;
+
+  /*DeedsPageState({this.deed, this.deedUUID}){
+    print(this.deedUUID);
+  }*/
+  DeedPageState({deed, deedUUID}){
+    this.deed = deed;
+    this.deedUUID = deedUUID;
+    print("DEED UUID : ");
+    print(deedUUID);
+  }
+
+  Future<void> _fetchDeed(String uuid) async {
+    String url = Globals.backendURL + '/deedsv2/deed/' + uuid;
+
+    var urlUri = Uri.parse(url);
+    final response = await http.Client().get(urlUri);
+    print(response.body);
+
+    final parsed = jsonDecode(response.body);
+    print(parsed);
+
+    setState(() {
+      deed = Deed.fromJson(parsed);
+      _loading = false;
+    });
+    print(deed);
+    print(deed.uuid);
+    print(deed.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //print('Deed UUID : ' + this.deedUUID);
+    _error = false;
+    _loading = true;
+    77_fetchDeed(this.deedUUID);
+
+
+    if(this.deed != null){
+      _loading = false;
+    } else if (this.deedUUID != null){
+      _fetchDeed(this.deedUUID);
+    } else {
+      //Oops! We shouldn't be here!
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: AppBar(
-          title: Text(deed.title),  //Don't need to worry about text overflow, handled already
+          //TODO check that just checking _loading is enough i.e. deed isn't null and _error isn't true!
+          title: Text(_loading ? 'Loading...' : deed.title),  //Don't need to worry about text overflow, handled already
         ),
         //drawer: GDDrawer(),
-        body: new DeedView(deed: this.deed,)
+        body: _loading ? Container(child: Center(child: new CircularProgressIndicator(),)) : new DeedView(deed: this.deed,)
     );
   }
 }
